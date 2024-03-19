da73e547c500be652bf897206d2ab7346e3f5cd6
olafbado <niebiel1@gmail.com>
2024-03-19 17:41:44 +0100

    test

--- lib/permit/permissions/condition_parser.ex
+++ lib/permit/permissions/condition_parser.ex
@@ -65,11 +65,16 @@ defmodule Permit.Permissions.ConditionParser do
       when operator in @operators and is_atom(key) and not is_nil(value) do
     case Operators.get(operator) do
       {:ok, module} ->
-        val_fn = binding_fn(value, Keyword.get(ops, :bindings))
+        val_fn =
+          if Keyword.keyword?(value),
+            do: value,
+            else: binding_fn(value, Keyword.get(ops, :bindings))
+
+        condition_type = if Keyword.keyword?(value), do: :association, else: :operator
 
         %ParsedCondition{
           condition: {key, val_fn},
-          condition_type: {:operator, module},
+          condition_type: {condition_type, module},
           semantics: module.semantics(val_fn, ops),
           not: Keyword.get(ops, :not, false)
         }
