c9fc2d0d90fbc1cfcc5b9a42a6b6c5b7ab8e7e45
Michał Buszkiewicz <michal@curiosum.com>
2024-07-30 12:22:36 +0200

    Remove unused versioce changelog generation

--- config/config.exs
+++ config/config.exs
@@ -9,19 +9,6 @@ config :versioce, :git,
   tag_template: "v{version}",
   tag_message_template: "Release v{version}"
 
-config :versioce, :changelog,
-  changelog_file: "CHANGELOG.md",
-  datagrabber: Versioce.Changelog.DataGrabber.Git,
-  formatter: Versioce.Changelog.Formatter.Keepachangelog,
-  anchors: %{
-    added: ["add:", "build:", "Add", "Introduce"],
-    changed: ["chore:", "refactor:", "feat:", "docs:", "ci:", "Update", "Refactor", "Rename"],
-    deprecated: [],
-    removed: ["revert:", "Remove"],
-    fixed: ["fix:", "perf:", "Fix", "Make up", "Restore"],
-    security: []
-  }
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
